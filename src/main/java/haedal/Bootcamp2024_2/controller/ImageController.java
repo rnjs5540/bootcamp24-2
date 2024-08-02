e434cd2c5a9adf98cf035285be74e3d131e87f18
yongminKwon1 <ym.kwon@lipis.kr>
2024-08-03 07:09:24 +0900

    Fix: 이미지 깨짐문제 해결

--- src/main/java/haedal/Bootcamp2024_2/controller/ImageController.java
+++ src/main/java/haedal/Bootcamp2024_2/controller/ImageController.java
@@ -6,12 +6,15 @@ import haedal.Bootcamp2024_2.service.ImageService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 public class ImageController {
@@ -35,8 +38,11 @@ public class ImageController {
     }
 
     @GetMapping("/images/{imageUrl}")
-    public ResponseEntity<Resource> getImage(@PathVariable String imageUrl) throws MalformedURLException {
+    public ResponseEntity<Resource> getImage(@PathVariable String imageUrl) throws IOException {
         Resource resource = imageService.loadImageAsResource(imageUrl);
-        return ResponseEntity.ok(resource);
+        Path imagePath = resource.getFile().toPath();
+        String contentType = Files.probeContentType(imagePath);
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
     }
 }
