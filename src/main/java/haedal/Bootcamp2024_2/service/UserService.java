48252ad0e782b6ea3ac819a14b99cc38343ae3eb
HyeonJeong <wgwjh05169@gmail.com>
2024-09-02 20:41:18 +0900

    User 서비스 작성

---
+++ src/main/java/haedal/Bootcamp2024_2/service/UserService.java
@@ -0,0 +1,37 @@
+package haedal.Bootcamp2024_2.service;
+
+import haedal.Bootcamp2024_2.domain.User;
+import haedal.Bootcamp2024_2.dto.response.UserSimpleResponseDto;
+import haedal.Bootcamp2024_2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserSimpleResponseDto saveUser(User newUser) {
+        // 중복 회원 검증
+        if (userRepository.existsByUsername(newUser.getUsername())) {
+            throw new IllegalStateException("중복되는 username입니다.");
+        }
+
+        userRepository.save(newUser);
+        return convertUserToSimpleDto(newUser, newUser);
+    }
+
+    public UserSimpleResponseDto convertUserToSimpleDto(User currentUser, User targetUser) {
+        return new UserSimpleResponseDto(
+                currentUser.getId(),
+                currentUser.getUsername(),
+                currentUser.getName(),
+                null,
+                false
+        );
+    }
+}
