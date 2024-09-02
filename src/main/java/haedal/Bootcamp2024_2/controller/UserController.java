5bdd29f14a50feaac997ad23bbe7503b0b407b97
HyeonJeong <wgwjh05169@gmail.com>
2024-09-02 21:09:26 +0900

    User 컨트롤러 작성

--- src/main/java/haedal/Bootcamp2024_2/controller/UserController.java
+++ src/main/java/haedal/Bootcamp2024_2/controller/UserController.java
@@ -1,17 +1,64 @@
 package haedal.Bootcamp2024_2.controller;
 
+import haedal.Bootcamp2024_2.domain.User;
+import haedal.Bootcamp2024_2.dto.request.UserUpdateRequestDto;
+import haedal.Bootcamp2024_2.dto.response.UserDetailResponseDto;
+import haedal.Bootcamp2024_2.dto.response.UserSimpleResponseDto;
+import haedal.Bootcamp2024_2.service.AuthService;
 import haedal.Bootcamp2024_2.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 public class UserController {
     private final UserService userService;
+    private final AuthService authService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
+    @GetMapping("/users")
+    public ResponseEntity<List<UserSimpleResponseDto>> getUsers(
+            @RequestParam(required = false) String username, HttpServletRequest request) {
+
+        User currentUser = authService.getCurrentUser(request);
+
+        List<UserSimpleResponseDto> users;
+        if (username == null || username.isEmpty()) {
+            users = userService.getAllUsers(currentUser);
+        } else {
+            users = userService.getUserByUsername(currentUser, username);
+        }
+
+        return ResponseEntity.ok(users);
+    }
+
+
+    @PutMapping("/users/profile")
+    public ResponseEntity<UserDetailResponseDto> updateUser(
+            @RequestBody UserUpdateRequestDto userUpdateRequestDto, HttpServletRequest request) {
+
+        User currentUser = authService.getCurrentUser(request);
+        UserDetailResponseDto updated = userService.updateUser(currentUser, userUpdateRequestDto);
+
+        return ResponseEntity.ok(updated);
+    }
+
+    @GetMapping("/users/{userId}/profile")
+    public ResponseEntity<UserDetailResponseDto> getUserProfile(@PathVariable Long userId, HttpServletRequest request) {
+
+        User currentUser = authService.getCurrentUser(request);
+
+        UserDetailResponseDto userDetailResponseDto = userService.getUserDetail(currentUser, userId);
+
+        return ResponseEntity.ok(userDetailResponseDto);
+    }
 }
