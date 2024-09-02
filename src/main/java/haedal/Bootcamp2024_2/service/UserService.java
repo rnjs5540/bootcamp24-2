ae8cb308758f61b4548287fce5acbe1dfd1e5827
HyeonJeong <wgwjh05169@gmail.com>
2024-09-02 21:09:15 +0900

    User 서비스 작성

--- src/main/java/haedal/Bootcamp2024_2/service/UserService.java
+++ src/main/java/haedal/Bootcamp2024_2/service/UserService.java
@@ -1,10 +1,16 @@
 package haedal.Bootcamp2024_2.service;
 
 import haedal.Bootcamp2024_2.domain.User;
+import haedal.Bootcamp2024_2.dto.request.UserUpdateRequestDto;
+import haedal.Bootcamp2024_2.dto.response.UserDetailResponseDto;
 import haedal.Bootcamp2024_2.dto.response.UserSimpleResponseDto;
 import haedal.Bootcamp2024_2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -32,6 +38,64 @@ public class UserService {
                 currentUser.getName(),
                 null,
                 false
+        );
+    }
+
+    public List<UserSimpleResponseDto> getAllUsers(User currentUser) {
+        List<User> users = userRepository.findAll();
+        users.remove(currentUser);
+        return users.stream().map(user -> convertUserToSimpleDto(currentUser, user)).toList();
+    }
+
+    public List<UserSimpleResponseDto> getUserByUsername(User currentUser, String username) {
+        List<UserSimpleResponseDto> user = new ArrayList<>();
+        User targetUser = userRepository.findByUsername(username).orElse(null);
+        if (targetUser != null) {
+            UserSimpleResponseDto userSimpleResponseDto = convertUserToSimpleDto(currentUser, targetUser);
+            user.add(userSimpleResponseDto);
+        }
+
+        return user;
+    }
+
+    public UserDetailResponseDto updateUser(User currentUser, UserUpdateRequestDto userUpdateRequestDto) {
+        if (userUpdateRequestDto.getUsername() != null) {
+            currentUser.setUsername(userUpdateRequestDto.getUsername());
+        }
+        if (userUpdateRequestDto.getPassword() != null) {
+            currentUser.setPassword(userUpdateRequestDto.getPassword());
+        }
+        if (userUpdateRequestDto.getName() != null) {
+            currentUser.setName(userUpdateRequestDto.getName());
+        }
+        if (userUpdateRequestDto.getBio() != null) {
+            currentUser.setBio(userUpdateRequestDto.getBio());
+        }
+
+        userRepository.save(currentUser);
+
+        return convertUserToDetailDto(currentUser, currentUser);
+    }
+
+
+    public UserDetailResponseDto getUserDetail(User currentUser, Long targetUserId) {
+        User targetUser = userRepository.findById(targetUserId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+        return convertUserToDetailDto(currentUser, targetUser);
+    }
+
+    public UserDetailResponseDto convertUserToDetailDto(User currentUser, User targetUser) {
+        return new UserDetailResponseDto(
+                targetUser.getId(),
+                targetUser.getUsername(),
+                targetUser.getName(),
+                null,
+                false,
+                targetUser.getBio(),
+                targetUser.getJoinedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")),
+                0L,
+                0L,
+                0L
         );
     }
 }
