f2727887b06bde4a4e75d63d43dbd492151b6446
HyeonJeong <wgwjh05169@gmail.com>
2024-09-02 20:53:37 +0900

    User repository에 findByUsername 메소드 작성

--- src/main/java/haedal/Bootcamp2024_2/repository/UserRepository.java
+++ src/main/java/haedal/Bootcamp2024_2/repository/UserRepository.java
@@ -4,7 +4,11 @@ import haedal.Bootcamp2024_2.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
