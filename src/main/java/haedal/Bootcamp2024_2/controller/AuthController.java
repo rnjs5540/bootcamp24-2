5f341fccd1dee7705b08164e6d1a3aace5309b54
HyeonJeong <wgwjh05169@gmail.com>
2024-09-02 20:52:40 +0900

    Auth controller 작성 및 User register API 이동

---
+++ src/main/java/haedal/Bootcamp2024_2/controller/AuthController.java
@@ -0,0 +1,59 @@
+package haedal.Bootcamp2024_2.controller;
+
+import haedal.Bootcamp2024_2.domain.User;
+import haedal.Bootcamp2024_2.dto.request.LoginRequestDto;
+import haedal.Bootcamp2024_2.dto.request.UserRegistrationRequestDto;
+import haedal.Bootcamp2024_2.dto.response.UserSimpleResponseDto;
+import haedal.Bootcamp2024_2.service.AuthService;
+import haedal.Bootcamp2024_2.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthController {
+    private final AuthService authService;
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(AuthService authService, UserService userService) {
+        this.authService = authService;
+        this.userService = userService;
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<UserSimpleResponseDto> registerUser(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto) {
+        User user = new User(
+                userRegistrationRequestDto.getUsername(),
+                userRegistrationRequestDto.getPassword(),
+                userRegistrationRequestDto.getName()
+        );
+        UserSimpleResponseDto savedUser = userService.saveUser(user);
+
+        return ResponseEntity.ok(savedUser);
+    }
+
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<UserSimpleResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletRequest request) {
+        UserSimpleResponseDto userSimpleResponseDto = authService.login(loginRequestDto, request);
+        return ResponseEntity.ok(userSimpleResponseDto);
+    }
+
+
+    @PostMapping("/auth/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        authService.logout(request);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @GetMapping("/auth/me")
+    public ResponseEntity<UserSimpleResponseDto> me(HttpServletRequest request) {
+        User currentUser = authService.getCurrentUser(request);
+
+        UserSimpleResponseDto userSimpleResponseDto = userService.convertUserToSimpleDto(currentUser, currentUser);
+        return ResponseEntity.ok(userSimpleResponseDto);
+    }
+}
--- src/main/java/haedal/Bootcamp2024_2/controller/UserController.java
+++ src/main/java/haedal/Bootcamp2024_2/controller/UserController.java
@@ -1,11 +1,7 @@
 package haedal.Bootcamp2024_2.controller;
 
-import haedal.Bootcamp2024_2.domain.User;
-import haedal.Bootcamp2024_2.dto.request.UserRegistrationRequestDto;
-import haedal.Bootcamp2024_2.dto.response.UserSimpleResponseDto;
 import haedal.Bootcamp2024_2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,15 +14,4 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/auth/register")
-    public ResponseEntity<UserSimpleResponseDto> registerUser(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto) {
-        User user = new User(
-                userRegistrationRequestDto.getUsername(),
-                userRegistrationRequestDto.getPassword(),
-                userRegistrationRequestDto.getName()
-        );
-        UserSimpleResponseDto savedUser = userService.saveUser(user);
-
-        return ResponseEntity.ok(savedUser);
-    }
 }
